e535d3c3d12fe762e7d75de65216750397b2f7ba
yogamaha94 <aadvya@gmail.com>
2026-06-19 09:22:36 +0530

    Implemented Login page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,20 +1,21 @@
 class LoginPage {
-  constructor(page) {
-    this.page = page;
-    this.username = page.locator('#username');
-    this.password = page.locator('#password');
-    this.loginButton = page.locator('input[value="Log In"]');
-  }
 
-  async goto() {
-    await this.page.goto('https://parabank.parasoft.com/parabank/login.htm');
-  }
+    constructor(page){
 
-  async login(credentials) {
-    await this.username.fill(credentials.username);
-    await this.password.fill(credentials.password);
-    await this.loginButton.click();
-  }
+        this.page = page;
+
+        this.username = page.locator("input[name='username']");
+        this.password = page.locator("input[name='password']");
+        this.loginBtn = page.locator("input[value='Log In']");
+    }
+
+    async login(username,password){
+
+        await this.username.fill(username);
+        await this.password.fill(password);
+
+        await this.loginBtn.click();
+    }
 }
 
 module.exports = LoginPage;
